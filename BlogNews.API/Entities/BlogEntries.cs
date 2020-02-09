7ddacd66d44d8c97eb7185b2d8bfb5eb02f033d7
Oscar Lagatta <oscarlagatta@gmail.com>
2020-02-09 21:42:54 +0000

    adding data store

---
+++ BlogNews.API/Entities/BlogEntries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BlogNews.API.Entities
+{
+    public class BlogEntries
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("BlogId")]
+        public Blog Blog { get; set; }
+
+        [MaxLength(1500)]
+        public string EntryText { get; set; }
+    }
+}
